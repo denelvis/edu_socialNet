375eeaeda66836673c32388b233424b7d5d5b1f1
Denis <denelvis@yandex.ru>
2021-09-10 20:58:21 +0300

    folder components

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img
+        src="https://toppng.com/uploads/preview/free-logo-design-11551057495oqoep79juj.png"
+        alt="logo"
+      />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img
+          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
+          alt="pic"
+        />
+      </div>
+      <div>
+        {/* <img
+  src="https://image.flaticon.com/icons/png/512/147/147144.png"
+  alt="avatar"
+/> */}
+        avatar + description
+      </div>
+      <div>
+        My posts
+        <div>New posts</div>
+        <div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
