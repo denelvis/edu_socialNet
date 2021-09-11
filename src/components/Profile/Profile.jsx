60dccf37d74dc7d5ef315bbf438060c2e178b92a
Denis <denelvis@yandex.ru>
2021-09-11 18:44:26 +0300

    added new component Dialogs, refactoring css

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,12 +4,11 @@ import classes from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={classes.content}>
+    <div>
       <div>
         <img
           src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
           alt="pic"
-          className={classes.img}
         />
       </div>
       <div>
