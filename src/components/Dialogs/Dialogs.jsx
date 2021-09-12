be427f09978fa5c0e2db57d7cac60964f2dbf29a
Denis <denelvis@yandex.ru>
2021-09-12 17:46:19 +0300

    added NavLink to Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <div className={`${classes.dialog} ${classes.active}`}>Dimych</div>
-        <div className={classes.dialog}>Andrey</div>
-        <div className={classes.dialog}>Sveta</div>
-        <div className={classes.dialog}>Sasha</div>
-        <div className={classes.dialog}>Victor</div>
-        <div className={classes.dialog}>Valera</div>
+        <div className={`${classes.dialog} ${classes.active}`}><NavLink to="/dialogs/1">Dimych</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/2">Andrey</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/3">Sveta</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/4">Sasha</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/5">Victor</NavLink></div>
+        <div className={classes.dialog}><NavLink to="/dialogs/6">Valera</NavLink></div>
       </div>
       <div className={classes.messages}>
         <div className={classes.message}>Hello</div>
