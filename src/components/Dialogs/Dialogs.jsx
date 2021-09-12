9cc1b56766caefaed76d1ef2e8da48f37090f5d2
Denis <denelvis@yandex.ru>
2021-09-12 19:05:37 +0300

    added ProfileInfo, refactoring Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,21 +2,36 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import classes from "./Dialogs.module.css";
 
+const DialogItems = (props) => {
+  const path = "/dialogs/" + props.id;
+  return (
+    <div className={classes.dialog}>
+      <NavLink to={path} activeClassName={classes.active}>
+        {props.name}
+      </NavLink>
+    </div>
+  );
+};
+
+const Message = (props) => {
+  return <div className={classes.message}>{props.message}</div>;
+};
+
 const Dialogs = (props) => {
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsItems}>
-        <div className={`${classes.dialog} ${classes.active}`}><NavLink to="/dialogs/1">Dimych</NavLink></div>
-        <div className={classes.dialog}><NavLink to="/dialogs/2">Andrey</NavLink></div>
-        <div className={classes.dialog}><NavLink to="/dialogs/3">Sveta</NavLink></div>
-        <div className={classes.dialog}><NavLink to="/dialogs/4">Sasha</NavLink></div>
-        <div className={classes.dialog}><NavLink to="/dialogs/5">Victor</NavLink></div>
-        <div className={classes.dialog}><NavLink to="/dialogs/6">Valera</NavLink></div>
+        <DialogItems name="Dimych" id="1" />
+        <DialogItems name="Andrey" id="2" />
+        <DialogItems name="Sveta" id="3" />
+        <DialogItems name="Sasha" id="4" />
+        <DialogItems name="Victor" id="5" />
+        <DialogItems name="Valera" id="6" />
       </div>
       <div className={classes.messages}>
-        <div className={classes.message}>Hello</div>
-        <div className={classes.message}>How does the training go?</div>
-        <div className={classes.message}>What's news?</div>
+        <Message message="Hi" />
+        <Message message="How does the training go?" />
+        <Message message="What's news?" />
       </div>
     </div>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import MyPosts from "./MyPosts/MyPosts";
 import classes from "./Profile.module.css";
+import ProfileInfo from "./ProfileInfo/ProfileInfo";
 
 const Profile = () => {
   return (
     <div>
-      <div>
-        <img
-          src="https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg"
-          alt="pic"
-        />
-      </div>
-      <div>
-        avatar + description
-      </div>
+      <ProfileInfo />
       <MyPosts />
     </div>
   );
