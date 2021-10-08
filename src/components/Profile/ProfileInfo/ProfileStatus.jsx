869dcc16e26b3ed44c007a6f22646a1d59c81e28
Denis <denelvis@yandex.ru>
2021-10-09 00:14:21 +0300

    fixed state flux

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -18,6 +18,13 @@ class ProfileStatus extends React.Component {
   onStatusChange = (e) => {
     this.setState({ status: e.currentTarget.value });
   };
+
+  componentDidUpdate(prevProps, prevStatus) {
+    if (prevProps.status !== this.props.status) {
+      this.setState({ status: this.props.status });
+    }
+  }
+
   render() {
     return (
       <div>
